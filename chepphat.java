194ef8b3449bfbd8897873c2fae2c31e09a97e15
chuong2110 <chuongbui2004@gmail.com>
2024-09-16 18:13:38 +0700

    Add files via upload

---
+++ chepphat.java
@@ -0,0 +1,9 @@
+
+
+public class chepphat {
+    public static void main(String[] args) {
+        for (int i=0; i<500 ; i++); 
+        System.out.println(".em hứa sẽ chép bài đầy đủ ");
+    }
+    
+}
